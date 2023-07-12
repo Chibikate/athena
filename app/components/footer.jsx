affd08ff9be007b0ed7ab453e263634b21b0def2
laurencetroyv-asus <laurencetroyv@gmail.com>
2023-07-12 12:50:29 +0800

    chore (update): create Footer Component

---
+++ app/components/footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className='bg-primary text-center text-white text-sm lg:text-base p-6'>
+      <p>
+        © Copyright 2023 <br />
+      </p>
+      <p className='font-bold'>
+        Web-based Tutorial on Virtual Planning for Reconstruction of Simple
+        Mandibular Fractures
+      </p>
+      <p>
+        Mindanao State University - Iligan Institute of Technology (MSU-IIT)
+      </p>
+      <p>Andres Bonifacio Avenue, Tibanga 9200 Iligan City, Philippines</p>
+    </footer>
+  );
+}
--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,7 @@ import Logo from "./assets/Logo.svg";
 import Link from "next/link";
 import { useState } from "react";
 import { ChevronDownIcon } from "@heroicons/react/24/solid";
+import Footer from "./components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 const anton = Anton({ weight: "400", subsets: ["latin"] });
@@ -19,10 +20,10 @@ export default function RootLayout({ children }) {
   const [isHovered, setIsHovered] = useState(false);
 
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={`${inter.className}`}>
-        <div className="flex flex-rows space-x-4 justify-between items-center bg-[#1d2d44] p-4">
-          <div className="flex flex-rows items-center space-x-4">
+        <div className='flex flex-rows space-x-4 justify-between items-center bg-[#1d2d44] p-4'>
+          <div className='flex flex-rows items-center space-x-4'>
             <Link href={"/"}>
               <Image src={Logo} />
             </Link>
@@ -37,27 +38,26 @@ export default function RootLayout({ children }) {
             </div>
           </div>
           <nav>
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-              <div className="flex items-center justify-between h-16">
-                <div className="ml-10 flex items-baseline space-x-4">
-                  <Link href="/">
-                    <p className="text-white hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+            <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
+              <div className='flex items-center justify-between h-16'>
+                <div className='ml-10 flex items-baseline space-x-4'>
+                  <Link href='/'>
+                    <p className='text-white hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium'>
                       Home
                     </p>
                   </Link>
 
-                  <Link href="/aboutus">
-                    <p className="text-white hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                  <Link href='/aboutus'>
+                    <p className='text-white hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium'>
                       About us
                     </p>
                   </Link>
 
                   <div
-                    className="relative group"
-                    onMouseEnter={() => setIsHovered(true)}
-                    onMouseLeave={() => setIsHovered(false)}
+                    className='relative group'
+                    onClick={() => setIsHovered(!isHovered)}
                   >
-                    <button className="text-white hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium flex flex-row items-center space-x-4">
+                    <button className='text-white hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium flex flex-row items-center space-x-4'>
                       Tutorials
                       <ChevronDownIcon
                         className={`h-4 w-4 ml-1 transition ${
@@ -65,20 +65,21 @@ export default function RootLayout({ children }) {
                         }`}
                       />
                     </button>
+
                     {isHovered && (
-                      <div className="absolute left-0 mt-2 py-2 w-48 bg-white shadow-lg rounded-md">
-                        <Link href="/thingstodownload">
-                          <p className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
+                      <div className='absolute right-2 mt-2 py-2 w-48 bg-white shadow-lg rounded-md'>
+                        <Link href='/thingstodownload'>
+                          <p className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100'>
                             Things to download
                           </p>
                         </Link>
-                        <Link href="/3dslicer">
-                          <p className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
+                        <Link href='/3dslicer'>
+                          <p className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100'>
                             3D slicer
                           </p>
                         </Link>
-                        <Link href="/meshmixer">
-                          <p className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
+                        <Link href='/meshmixer'>
+                          <p className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100'>
                             Meshmixer
                           </p>
                         </Link>
@@ -90,20 +91,8 @@ export default function RootLayout({ children }) {
             </div>
           </nav>
         </div>
-        <div className="pt-4">{children}</div>
-        <footer className="bg-primary text-center text-white">
-          <p className="py-6 text-sm">
-            © Copyright 2023 <br />{" "}
-            <span className="font-bold">
-              Web-based Tutorial on Virtual Planning for Reconstruction of
-              Simple Mandibular Fractures
-            </span>
-            <br />
-            Mindanao State University - Iligan Institure of Technology (MSU-IIT)
-            <br />
-            Andres Bonifacio Avenue, Tibanga 9200 Iligan City, Philippines
-          </p>
-        </footer>
+        <div className='pt-4'>{children}</div>
+        <Footer />
       </body>
     </html>
   );
