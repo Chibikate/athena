b37d21cb51c79631cd1cce1a4675c43341802051
Chibikate <queenie.cabanilla@gmail.com>
2023-09-14 11:54:28 +0800

    ok

--- app/mandiweb/3dslicer/page.js
+++ app/mandiweb/3dslicer/page.js
@@ -1,7 +1,7 @@
 import React from "react";
-import Image from "next/image";
-import Image1 from "./assets/Image1.png";
 import Link from "next/link";
+import Image from "next/image";
+import Image1 from "./assets/image-1.png";
 
 function ListItem({ text }) {
   return <li className="list-disc ml-6">{text}</li>;
@@ -12,18 +12,16 @@ export default function Page() {
     <div className="bg-white rounded-lg shadow-md p-6">
       <div className="flex items-center space-x-4">
         <div className="w-1/2">
-          <Image src={Image1} width={450} height={300} alt="Image Description" />
+          <Image src={Image1} width={800} height={300} alt="Image Description" />
         </div>
         <div className="w-1/2">
           <h2 className="text-3xl font-bold mb-2">Conversion of DICOM to STL</h2>
           <div className="h-1 bg-gray-300"></div>
           <ul className="mt-4 space-y-2">
-            <ListItem text="Discover how to navigate Meshmixer's user interface." />
-            <ListItem text="Familiarize with navigating the 3D environment in Meshmixer." />
-            <ListItem text="Learn how to use face groups and paint selection in separation of the mandible." />
-            <ListItem text="Perform 3D file manipulation in Meshmixer." />
-            <ListItem text="Utilize the plane cut function for editing." />
-            <ListItem text="Apply the mirroring function in Meshmixer." />
+            <ListItem text="Discover how to navigate the 3D slicer's user interface." />
+            <ListItem text="Export DICOM file to .STL" />
+            <ListItem text="Perform threshold-based segmentation to separate the bones" />
+            <ListItem text="Apply cropping to narrow down the region of interest" />
           </ul>
         </div>
       </div>
