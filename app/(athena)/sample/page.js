5806bb8cab97985ee6bfc0b24f1ae06f1194a17f
laurencetroyv-asus <laurencetroyv@gmail.com>
2023-11-01 21:44:04 +0800

    chore: Fix styling to center

--- app/(athena)/sample/page.js
+++ app/(athena)/sample/page.js
@@ -8,30 +8,36 @@ import Card from "@/components/card";
 export default function Page() {
   return (
     <div>
-      <div className="flex flex-row space-x-16 justify-end">
+      <Image
+        className="absolute -z-10"
+        alt="Somethings never change"
+        src={bg}
+        width={1920}
+        height={900}
+      />
+
+      <div className="flex flex-row space-x-16 justify-end p-4">
         <p>Home</p>
         <p>Contact Us</p>
         <p>Courses</p>
       </div>
 
-      
-
-      <div className="flex flex-row sm:flex-col items-center">
-        <div className="text-center justify-between ">
-          <p className="text-5xl font-bold text-primary pt-16 p-4">
-            Develop Your Skills with Our Tutorial Website<br />{" "}
-            <span className="text-[#FF5C00]">Your Journey Begins Now</span>
-          </p>
+      <div className="flex flex-row sm:flex-col min-h-screen justify-center items-center">
+        <div className="flex- flex-col ">
+          <div className="text-center justify-between items-center text-5xl font-bold text-white space-y-4 pt-6">
+            <p className="px-2 bg-green-500 rounded-md">
+              Develop Your Skills with Our Tutorial Website
+            </p>
+            <p className="text-[#FF5C00] rounded-md">Your Journey Begins Now</p>
+          </div>
+          <div className="text-center justify-between p-2">
+            <p className="text-gray-400">Explore free courses to learn</p>
+          </div>
         </div>
-        <div className="text-center justify-between p-2">
-          <p className="text-gray-400">Explore free courses to learn</p>
-        </div>
-
-        <Image className="absolute top-44" alt="Somethings never change" src={bg} width={1920} height={900} />
       </div>
 
       <div className="flex flex-col p-4">
-        <p className="text-3xl font-bold text-primary pt-28 pb-4">Explore Courses</p>
+        <p className="text-3xl font-bold text-primary">Explore Courses</p>
         <div className="flex flex-row space-x-4">
           <VirtualSurgicalPlanning />
         </div>
